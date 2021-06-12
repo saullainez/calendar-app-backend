6e88d40e5556185805557cc8e10b064694c5af40
Saúl Laínez <saullainez@hotmail.es>
2021-06-12 08:35:05 -0600

    Configurar express

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+//Creaer el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true
+    })
+
+});
+
+//Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`)
+});
