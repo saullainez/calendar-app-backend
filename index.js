43b1a13bef73c6168e25ed5cdc4d5fb274a29bdd
Saúl Laínez <saullainez@hotmail.es>
2021-06-15 08:06:19 -0600

    Recuperar información de un posteo

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 //Directorio público
 app.use( express.static('public') );
 
+//Lectura y parseo del body
+app.use( express.json() );
+
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
 //TODO: CRUD: eventos
