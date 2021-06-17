4a316f2e0bd3b0e4badfee00785c1df4a69935c8
Saúl Laínez <saullainez@hotmail.es>
2021-06-17 08:20:11 -0600

    Conectar Node a Mongo Atlas

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
 //Creaer el servidor de express
 const app = express();
+
+//Base de datos
+dbConnection();
 
 //Directorio público
 app.use( express.static('public') );
