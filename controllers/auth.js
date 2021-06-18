12eb29659cbdb3358380ff73794eb514371398fa
Saúl Laínez <saullainez@hotmail.es>
2021-06-18 07:57:15 -0600

    Encriptar la contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const createUser = async(req, res = response) => {
@@ -17,6 +18,10 @@ const createUser = async(req, res = response) => {
         }
         
         user = new User( req.body );
+
+        //Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
 
         await user.save();
     
