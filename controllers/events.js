6356f6843e452af19bddef7064d22b0799c4030f
Saúl Laínez <saullainez@hotmail.es>
2021-06-23 09:10:57 -0600

    Guardar el evento en la base de datos.

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require('../models/Event');
 
 const getEvents = (req, res = response) => {
 
@@ -9,12 +10,28 @@ const getEvents = (req, res = response) => {
 
 }
 
-const createEvent = (req, res = response) => {
+const createEvent = async (req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'CreateEvent'
-    });
+    const event = new Event( req.body );
+
+    try {
+
+        event.user = req.uid;
+
+        const savedEvent = await event.save();
+
+        res.status(201).json({
+            ok: true,
+            event: savedEvent
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Ocurrió un error, hable con el administrador.'
+        })
+    }
 
 }
 
